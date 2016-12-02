a473f5b0f8fe5470088593401c53e72a83ac4e0a
crosanom <crosanom@hotmail.com>
2016-12-02 14:50:11 +0100

    Esto es el segundo intento

--- src/hola.java
+++ src/hola.java
@@ -1,23 +1,44 @@
 import java.util.Scanner;
+
 public class hola {
+	
+		private int edad;
+		private String nombre;
+
+		public int getEdad() {
+			return edad;
+		}
+
+		public void setEdad(int edad) {
+			this.edad = edad;
+		}
+
+		public String getNombre() {
+			return nombre;
+		}
+
+		public void setNombre(String nombre) {
+			this.nombre = nombre;
+		}
+
+		
+		
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("hola");
-		
-			System.out.println("Cris");
-			System.out.println("introduce un nombre");
-			
-			String nombre;
-			Scanner sc=new Scanner(System.in);
-			nombre=sc.next();
-			System.out.println("nombre");
-			System.out.println("Hola"+nombre);
-			
-			
-			
 
-}
-	
+		System.out.println("Cris");
+		System.out.println("introduce un nombre");
+
+		String nombre;
+		Scanner sc = new Scanner(System.in);
+		nombre = sc.next();
+		System.out.println("nombre");
+		System.out.println("Hola" + nombre);
+
+	}
+
 }
